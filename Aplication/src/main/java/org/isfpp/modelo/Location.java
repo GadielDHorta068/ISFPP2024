87a97070cda09aadac2f95653159cd12ebd93d5a
mikan11037 <michiaguilarr@gmail.com>
2024-09-25 18:24:29 -0300

    modelo

---
+++ Aplication/src/main/java/org/isfpp/modelo/Location.java
@@ -0,0 +1,48 @@
+package modelo;
+
+import java.util.Objects;
+
+public class Location {
+	private String code;
+	private String description;
+	public Location(String code, String description) {
+		super();
+		this.code = code;
+		this.description = description;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(code);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Location other = (Location) obj;
+		return Objects.equals(code, other.code);
+	}
+	@Override
+	public String toString() {
+		return "Location [code=" + code + ", description=" + description + "]";
+	}
+	
+	
+	
+
+}
---
+++ Aplication/src/main/java/org/isfpp/modelo/Web.java
@@ -0,0 +1,13 @@
+package modelo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
+public class Web {
+	private HashMap<String,Equipment> hardware;
+	private ArrayList<link> linked;
+	private HashMap<String,Location> locations;
+	private String nombre;
+
+}
